edac16435f6df88dcc2978c0f92b2a5c7b82e4d3
yongbinkwon <yong.bin.kwon@nav.no>
2024-12-05 15:46:09 +0100

    Legger på test-test :))
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>

---
+++ app/src/test/kotlin/no/nav/aap/meldekort/Test.kt
@@ -0,0 +1,12 @@
+package no.nav.aap.meldekort
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class Test {
+
+    @Test
+    fun tester() {
+        assertTrue(true)
+    }
+}
