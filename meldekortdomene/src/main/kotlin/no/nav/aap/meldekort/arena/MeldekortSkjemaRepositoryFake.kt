bf5cffbd49e0755912c670c53e639c3afa41ebb4
yongbinkwon <yong.bin.kwon@nav.no>
2024-12-12 13:53:20 +0100

    Lagre bare siste meldekorttilstand per meldekort
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>

--- meldekortdomene/src/main/kotlin/no/nav/aap/meldekort/arena/MeldekortSkjemaRepositoryFake.kt
+++ meldekortdomene/src/main/kotlin/no/nav/aap/meldekort/arena/MeldekortSkjemaRepositoryFake.kt
@@ -2,25 +2,14 @@ package no.nav.aap.meldekort.arena
 
 /* TODO: Flytt til test-mappen når serveren ikke lenger bruker fake versjonen. */
 class MeldekortSkjemaRepositoryFake: MeldekortSkjemaRepository {
-    private val meldekorttilstander: MutableList<MeldekorttilstandEntity> = mutableListOf()
+    private var meldekorttilstand: Meldekorttilstand? = null
 
     override fun loadMeldekorttilstand(meldekortId: Long, flyt: Flyt): Meldekorttilstand? {
-        return meldekorttilstander.singleOrNull { it.meldekortId == meldekortId && it.aktiv }?.tilDomene(flyt)
-    }
-
-    override fun storeMeldekorttilstand(meldekorttilstand: Meldekorttilstand): Meldekorttilstand {
-        setToInactive()
-        this.meldekorttilstander.add(
-            MeldekorttilstandEntity.fraDomene(meldekorttilstand = meldekorttilstand, aktiv = true)
-        )
         return meldekorttilstand
     }
 
-    private fun setToInactive() {
-        val aktivTilstand = meldekorttilstander.single { it.aktiv }
-
-        meldekorttilstander.apply {
-            set(this.indexOf(aktivTilstand), aktivTilstand.copy(aktiv = false))
-        }
+    override fun storeMeldekorttilstand(meldekorttilstand: Meldekorttilstand): Meldekorttilstand {
+        this.meldekorttilstand = meldekorttilstand
+        return meldekorttilstand
     }
 }
