93c022baf5e885049db14acacf4e410f75091785
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-01-24 15:33:11 +0100

    Set message for ArenaInnsendingFeiletException

--- meldekortdomene/src/main/kotlin/no/nav/aap/meldekort/arena/ArenaInnsendingFeiletException.kt
+++ meldekortdomene/src/main/kotlin/no/nav/aap/meldekort/arena/ArenaInnsendingFeiletException.kt
@@ -4,7 +4,7 @@ data class ArenaInnsendingFeiletException(
     val kontrollStatus: String,
     val innsendingFeil: List<InnsendingFeil>,
     val skjema: Utfylling? = null,
-): Exception() {
+) : Exception("kontrollStatus=${kontrollStatus} feil=${innsendingFeil.joinToString { it.kode }}") {
     data class InnsendingFeil(
         val kode: String,
         val params: List<String>? = null
