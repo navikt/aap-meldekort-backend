06f269aa7eb0b957b7543338779a7b22814c5ae0
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-02-14 19:20:35 +0100

    Skjelett for FakeArena

--- lib-test/src/main/kotlin/no/nav/aap/meldekort/test/FakeArena.kt
+++ lib-test/src/main/kotlin/no/nav/aap/meldekort/test/FakeArena.kt
@@ -1,6 +1,10 @@
 package no.nav.aap.meldekort.test
 
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 object FakeArena: FakeServer {
     override fun setProperties(port: Int) {
@@ -10,5 +14,35 @@ object FakeArena: FakeServer {
         System.setProperty("meldekortkontroll.scope", "api://meldekortservice:aap:api-intern/.default")
     }
 
-    override val module: Application.() -> Unit = {}
+    override val module: Application.() -> Unit = {
+        install(ContentNegotiation) {
+            jackson()
+        }
+
+        routing {
+            get("/v2/meldegrupper") {
+                call.respondText("{}")
+            }
+
+            get("/v2/meldekort") {
+                call.respondText("{}")
+            }
+
+            get("/v2/historiskemeldekort") {
+                call.respondText("{}")
+            }
+
+            get("/v2/meldekortdetaljer") {
+                call.respondText("{}")
+            }
+
+            get("/v2/korrigertMeldekort") {
+                call.respondText("{}")
+            }
+
+            post("/api/v1/kontroll") {
+                call.respondText("{}")
+            }
+        }
+    }
 }
