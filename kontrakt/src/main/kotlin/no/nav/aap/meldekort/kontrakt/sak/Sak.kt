d3e29268f44d8a4b9c982dc4f13d87e0a3fd4eab
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-03-21 09:23:23 +0100

    Legg på identer i sak

--- kontrakt/src/main/kotlin/no/nav/aap/meldekort/kontrakt/sak/Sak.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/meldekort/kontrakt/sak/Sak.kt
@@ -3,6 +3,7 @@ package no.nav.aap.meldekort.kontrakt.sak
 import no.nav.aap.meldekort.kontrakt.Periode
 
 public class MeldeperioderV0(
+    public val identer: List<String>,
     public val saksnummer: String,
     public val sakenGjelderFor: Periode,
     public val meldeperioder: List<Periode>,
