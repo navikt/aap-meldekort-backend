cfa05782da6947aaec12667b94d564d2b60fc3a2
Matias Vinjevoll <matias.vinjevoll@nav.no>
2025-06-26 10:57:57 +0200

    Ikke close fake servers i tester Når fake servers deles på tvers av tester kan de bli skrudd av før alle testene er ferdig. Legg heller inn en shutdown hook

--- lib-test/src/main/kotlin/no/nav/aap/meldekort/test/FakeServers.kt
+++ lib-test/src/main/kotlin/no/nav/aap/meldekort/test/FakeServers.kt
@@ -14,11 +14,15 @@ interface FakeServer {
     val port: Int get() = 0
 }
 
-class FakeServers : AutoCloseable {
+object FakeServers : AutoCloseable {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     private val fakeServers = listOf(FakeTokenX, FakeAzure, FakeAapApi, FakeArena, FakeDokarkiv, FakePdfgen)
         .map { it to embeddedServer(Netty, port = it.port, module = it.module) }
+
+    init {
+        Runtime.getRuntime().addShutdownHook(Thread { close() })
+    }
 
     private val started = AtomicBoolean(false)
 
