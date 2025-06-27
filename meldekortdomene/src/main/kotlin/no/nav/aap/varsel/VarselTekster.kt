d6ee2736563acadfd35c06ea747ec5b87aad2ad4
Matias Vinjevoll <matias.vinjevoll@nav.no>
2025-06-27 14:49:23 +0200

    TODO legg inn tekster

--- meldekortdomene/src/main/kotlin/no/nav/aap/varsel/VarselTekster.kt
+++ meldekortdomene/src/main/kotlin/no/nav/aap/varsel/VarselTekster.kt
@@ -6,8 +6,11 @@ data class VarselTekster(
     val en: String
 )
 
+// TODO legg inn tekster
 val TEKSTER_BESKJED_FREMTIDIG_OPPLYSNINGSBEHOV = VarselTekster(nb = "", nn = "", en = "")
 
+// TODO legg inn tekster
 val TEKSTER_OPPGAVE_OPPLYSNINGSBEHOV = VarselTekster(nb = "", nn = "", en = "")
 
+// TODO legg inn tekster
 val TEKSTER_OPPGAVE_MELDEPLIKTPERIODE = VarselTekster(nb = "", nn = "", en = "")
