b9398d868c297b750a03306b0fd9727986e21c08
Matias Vinjevoll <matias.vinjevoll@nav.no>
2025-06-30 14:05:24 +0200

    Test for hentIdenter

--- repositories/src/test/kotlin/no/nav/aap/meldekort/kelvin/KelvinSakRepositoryPostgresTest.kt
+++ repositories/src/test/kotlin/no/nav/aap/meldekort/kelvin/KelvinSakRepositoryPostgresTest.kt
@@ -9,6 +9,7 @@ import no.nav.aap.komponenter.dbtest.InitTestDatabase
 import no.nav.aap.sak.FagsakReferanse
 import no.nav.aap.sak.Fagsaknummer
 import no.nav.aap.sak.FagsystemNavn
+import org.assertj.core.api.Assertions.assertThat
 import java.time.LocalDate
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -31,8 +32,24 @@ class KelvinSakRepositoryPostgresTest {
         InitTestDatabase.freshDatabase().transaction { connection ->
             val repo = KelvinSakRepositoryPostgres(connection)
 
-            repo.upsertSak(sak1, periode4, listOf(fnr1), listOf(), listOf(periode3), listOf(periode4), KelvinSakStatus.UTREDES)
-            repo.upsertSak(sak2, periode4, listOf(fnr3), listOf(periode1, periode2), listOf(), listOf(), KelvinSakStatus.AVSLUTTET)
+            repo.upsertSak(
+                sak1,
+                periode4,
+                listOf(fnr1),
+                listOf(),
+                listOf(periode3),
+                listOf(periode4),
+                KelvinSakStatus.UTREDES
+            )
+            repo.upsertSak(
+                sak2,
+                periode4,
+                listOf(fnr3),
+                listOf(periode1, periode2),
+                listOf(),
+                listOf(),
+                KelvinSakStatus.AVSLUTTET
+            )
 
             assertEquals(listOf(), repo.hentMeldeperioder(sak1))
             assertEquals(listOf(periode3), repo.hentMeldeplikt(sak1))
@@ -49,7 +66,15 @@ class KelvinSakRepositoryPostgresTest {
                 assertEquals(KelvinSakStatus.AVSLUTTET, it?.status)
             }
 
-            repo.upsertSak(sak1, periode2, listOf(fnr1, fnr2), listOf(periode1, periode3), listOf(), listOf(), KelvinSakStatus.LØPENDE)
+            repo.upsertSak(
+                sak1,
+                periode2,
+                listOf(fnr1, fnr2),
+                listOf(periode1, periode3),
+                listOf(),
+                listOf(),
+                KelvinSakStatus.LØPENDE
+            )
 
             assertEquals(listOf(periode1, periode3), repo.hentMeldeperioder(sak1))
             assertEquals(listOf(periode2, periode1), repo.hentMeldeperioder(sak2))
@@ -65,7 +90,15 @@ class KelvinSakRepositoryPostgresTest {
                 assertEquals(KelvinSakStatus.AVSLUTTET, it?.status)
             }
 
-            repo.upsertSak(sak1, periode2, listOf(fnr1, fnr2), listOf(periode2, periode3), listOf(), listOf(), KelvinSakStatus.AVSLUTTET)
+            repo.upsertSak(
+                sak1,
+                periode2,
+                listOf(fnr1, fnr2),
+                listOf(periode2, periode3),
+                listOf(),
+                listOf(),
+                KelvinSakStatus.AVSLUTTET
+            )
             assertEquals(listOf(periode2, periode3), repo.hentMeldeperioder(sak1))
             assertEquals(listOf(periode2, periode1), repo.hentMeldeperioder(sak2))
             repo.hentSak(fnr1, periode2.fom).also {
@@ -78,6 +111,34 @@ class KelvinSakRepositoryPostgresTest {
                 assertEquals(periode4, it?.rettighetsperiode)
                 assertEquals(KelvinSakStatus.AVSLUTTET, it?.status)
             }
+        }
+    }
+
+    @Test
+    fun `hent identer basert på sak`() {
+        InitTestDatabase.freshDatabase().transaction { connection ->
+            val repo = KelvinSakRepositoryPostgres(connection)
+            repo.upsertSak(
+                sak1,
+                periode4,
+                listOf(fnr1, fnr3),
+                listOf(),
+                listOf(periode3),
+                listOf(periode4),
+                KelvinSakStatus.UTREDES
+            )
+            repo.upsertSak(
+                sak2,
+                periode4,
+                listOf(fnr2),
+                listOf(periode1, periode2),
+                listOf(),
+                listOf(),
+                KelvinSakStatus.LØPENDE
+            )
+
+            assertThat(repo.hentIdenter(sak1)).containsExactlyInAnyOrder(fnr1, fnr3)
+            assertThat(repo.hentIdenter(sak2)).containsExactlyInAnyOrder(fnr2)
         }
     }
 }
