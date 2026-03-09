dbc77c6d418b8a92867d6430574d63f1d821e775
Fredrik Meyer <fredrik.meyer@nav.no>
2026-03-09 11:56:08 +0100

    Feilaktig skråstrek i URL (#562)

--- meldekortdomene/src/main/kotlin/no/nav/aap/sak/SakerService.kt
+++ meldekortdomene/src/main/kotlin/no/nav/aap/sak/SakerService.kt
@@ -1,7 +1,6 @@
 package no.nav.aap.sak
 
 import no.nav.aap.Ident
-import no.nav.aap.InnloggetBruker
 import no.nav.aap.kelvin.KelvinSakRepository
 import no.nav.aap.komponenter.repository.RepositoryProvider
 import no.nav.aap.lookup.gateway.GatewayProvider
@@ -15,10 +14,6 @@ class SakerService(
         aapGateway = gatewayProvider.provide(),
         kelvinSakRepository = repositoryProvider.provide(),
     )
-
-    fun finnSak(innloggetBruker: InnloggetBruker, påDag: LocalDate): Sak? {
-        return finnSak(innloggetBruker.ident, påDag)
-    }
 
     fun finnSak(ident: Ident, påDag: LocalDate): Sak? {
         val kelvinSak = kelvinSakRepository.hentSak(ident, påDag)
